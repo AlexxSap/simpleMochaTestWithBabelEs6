3193649f9895e479a553cccb5c50701b736d75a3
Александр Сапожников (Aleksandr Sapozhnikov) <alexxandrsap@mail.ru>
2017-05-01 21:16:11 +0300

    служебные функции переисаны на объект

--- myTest.js
+++ myTest.js
@@ -1,70 +1,77 @@
 'use strict';
 
-function assert(value1, value2)
+function _t()
 {
-  if(value1 === value2)
+  let globalAll = 0;
+  let globalFail = 0;
+  let globalPass = 0;
+
+  let currentName = '';
+  let currentAll = 0;
+  let currentFail = 0;
+  let currentPass = 0;
+
+  function assert(value1, value2)
   {
-    return 0;
+    if(value1 === value2)
+    {
+      return 0;
+    }
+    return 1;
+  };
+
+  this.runTest = function(name, actual, expected)
+  {
+    const result = assert(actual, expected);
+    if(result === 0)
+    {
+      console.log("test " + name + " done");
+      currentPass++;
+    }
+    else
+    {
+      console.log("test " + name + " fail!");
+      currentFail++;
+    }
+    currentAll++;
+  };
+
+  this.startTests = function(name)
+  {
+    currentAll = 0;
+    currentFail = 0;
+    currentPass = 0;
+    currentName = name;
+    console.log('start tests ' + name);
   }
-  return 1;
+
+  this.endTests = function()
+  {
+    globalAll += currentAll;
+    globalFail += currentFail;
+    globalPass += currentPass;
+    console.log("test " + currentName +
+    " pass="+ currentPass +
+    " fail=" + currentFail);
+  }
+
+  this.startGlobalTests = function()
+  {
+    globalAll = 0;
+    globalFail = 0;
+    globalPass = 0;
+    console.log('start tests');
+  }
+
+  this.endGlobalTests = function()
+  {
+    console.log("all test=" + globalAll +
+    " pass="+ globalPass +
+    " fail=" + globalFail);
+  }
 };
 
-var currentAll = 0;
-var currentFail = 0;
-var currentPass = 0;
-
-export function runTest(name, actual, expected)
+export function Test()
 {
-  const result = assert(actual, expected);
-  if(result === 0)
-  {
-    console.log("test " + name + " done");
-    currentPass++;
-  }
-  else
-  {
-    console.log("test " + name + " fail!");
-    currentFail++;
-  }
-  currentAll++;
+  return new _t();
 };
-
-var currentName = '';
-
-export function startTests(name)
-{
-  currentAll = 0;
-  currentFail = 0;
-  currentPass = 0;
-  currentName = name;
-  console.log('start tests ' + name);
-}
-
-export function endTests()
-{
-  globalAll += currentAll;
-  globalFail += currentFail;
-  globalPass += currentPass;
-  console.log("test " + currentName +
-  " pass="+ currentPass +
-  " fail=" + currentFail);
-}
-
-var globalAll = 0;
-var globalFail = 0;
-var globalPass = 0;
-
-export function startGlobalTests()
-{
-  globalAll = 0;
-  globalFail = 0;
-  globalPass = 0;
-  console.log('start tests');
-}
-
-export function endGlobalTests()
-{
-  console.log("all test=" + globalAll +
-  " pass="+ globalPass + 
-  " fail=" + globalFail);
-}
--- test.js
+++ test.js
@@ -1,31 +1,14 @@
 'use strict';
 
-//
-// const {describe, it} = require('mocha');
-// const {expect} = require('chai');
-
 import {sum} from "./index";
-import {runTest, startGlobalTests, endGlobalTests, startTests, endTests} from "./myTest";
+import {Test} from "./myTest";
 
-startGlobalTests();
-startTests('sum');
-runTest('simple1', sum(1, 3), 4);
-runTest('simple2', sum(1, 4), 4);
-runTest('simple3', sum(6, 0), 6);
-endTests();
-endGlobalTests();
+let $ = Test();
 
-
-//
-// describe("sum", function()
-// {
-//   it('simple1', function()
-//   {
-//     assert.equal(module.exports.sum(1, 3), 4);
-//   });
-//   it('simple2', function()
-//   {
-//     assert.equal(module.exports.sum(6, 0), 6);
-//   });
-//
-// });
+$.startGlobalTests();
+$.startTests('sum');
+$.runTest('simple1', sum(1, 3), 4);
+$.runTest('simple2', sum(1, 4), 4);
+$.runTest('simple3', sum(6, 0), 6);
+$.endTests();
+$.endGlobalTests();
